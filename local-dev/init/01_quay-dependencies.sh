f3c54bff3a8507695a1419292403462d8c3fc9ed
Jonathan King <jonathankingfc@gmail.com>
2020-12-01 22:50:14 -0500

    got frontend working

--- local-dev/init/01_quay-dependencies.sh
+++ local-dev/init/01_quay-dependencies.sh
@@ -6,12 +6,6 @@ QUAYDIR=${QUAYDIR:-"/"}
 
 cd $QUAYDIR 
 
-echo "[Local Dev] - Install Python Dependencies..."
-alternatives --set python /usr/bin/python3 && \
-    python -m pip install --no-cache-dir --upgrade setuptools pip && \
-    python -m pip install --no-cache-dir -r requirements.txt --no-cache && \
-    python -m pip freeze
-
 echo "[Local Dev] - Building Front End..."
 mkdir -p $QUAYDIR/static/webfonts && \
     mkdir -p $QUAYDIR/static/fonts && \
